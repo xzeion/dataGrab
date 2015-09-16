379d715eadd558285f7ee883130993bc722578a7
U-MBSINTL\bgratton <bgratton@LPDTW1006614.americas.media.global.loc>
2015-09-15 20:51:03 -0400

    First working version

--- background.js
+++ background.js
@@ -1,9 +1,9 @@
 /* Regex pattern to check URLs against.
    It matches URLs like: http[s]://[...]test.com[...] */
 
-var urlRegex = /^https?:\/\/(?:[^\.]+\.)?google\.com/dfa/;
+//var urlRegex = .*;
 
-/* A function creator for callbacks */
+// A function creator for callbacks
 function doStuffWithDOM(domContent){
 	console.log("I got the following DOM content:\n" + domContent);
 }
@@ -11,9 +11,9 @@ function doStuffWithDOM(domContent){
 /*When the browser-action button is clicked..*/
 chrome.browserAction.onClicked.addListener(function(tab) {
 	/*...check the URL of the actie tab against our pattern and...*/
-	if(urlRegex.test(tab.url)) {
+	//if(urlRegex.test(tab.url)) {
 		/*...if it matches, send a message specifying a callback too */
 		chrome.tabs.sendMessage(tab.id, { text: "report_back" },
-					doStuffWithDOM());
-	}
+					doStuffWithDOM);
+	//}
 });
--- content.js
+++ content.js
@@ -1,14 +1,9 @@
 /*Listen for messages*/
-
-var name = document.getElementById('span');
-console.log(name);
-
-/*
 chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
 	if (msg.text && (msg.text == "report_back")) {
 		//Call the specified callback, passing
-		the web-pages DOM content as an argument
-		sendResponse(document.all[0].outerHTML);
+		//the web-pages DOM content as an argument
+		sendResponse(document.URL);
 	}
 });
-*/
+
